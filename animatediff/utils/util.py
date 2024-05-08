b9dbab899f75337ec3892f31e29bd1b535911ab2
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-05-08 17:49:51 +0900

    unexpected

--- animatediff/utils/util.py
+++ animatediff/utils/util.py
@@ -115,10 +115,11 @@ def load_weights(
         motion_module_state_dict = motion_module_state_dict["state_dict"] if "state_dict" in motion_module_state_dict else motion_module_state_dict
         unet_state_dict.update({name: param for name, param in motion_module_state_dict.items() if "motion_modules." in name})
         unet_state_dict.pop("animatediff_config", "")
-    
+
+    # why there are unexpected >>>
     missing, unexpected = animation_pipeline.unet.load_state_dict(unet_state_dict, strict=False)
     print(f'missing = {len(missing)}')
-    print(f'unexpected = {len(unexpected)}')
+    print(f'unexpected = {unexpected}')
     assert len(unexpected) == 0
     del unet_state_dict
 
