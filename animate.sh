97ba519524b22541f6c57c7718c6860327faab21
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-05-08 21:20:02 +0900

    inference check

--- animate.sh
+++ animate.sh
@@ -1,16 +1,16 @@
 # -m = run library module as a script
 # config = about inference things
 # inference-config = about unet structure
+#--FPS ${Frames_per_second} \
 
 number_of_frame=16
 Frames_per_second=8
 
 python scripts/animate.py \
- --pretrained-model-path "../../pretrained/models/StableDiffusion/BambaBaby" \
- --config configs/prompts/v3/v3-BambaBaby.yaml \
+ --pretrained-model-path "../../pretrained/stable-diffusion-v1-5" \
+ --config configs/prompts/v4/v4-1-mistoon.yaml \
  --inference-config configs/inference/inference-v1.yaml \
- --H 128 \
+ --H 256 \
  --W 256 \
  --L ${number_of_frame} \
- --FPS ${Frames_per_second} \
  --without-xformers
