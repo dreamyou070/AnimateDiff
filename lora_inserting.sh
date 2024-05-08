a89bd975076bc5caa4ec0593ef4d2d67f2744af6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-05-08 14:38:35 +0900

    lora name changing test

--- lora_inserting.sh
+++ lora_inserting.sh
@@ -1,4 +1,4 @@
 python lora_inserting.py \
  --base_model_path ../../pretrained_stable_diffusion/stable-diffusion-v1-5 \
- --checkpoint_path ../../pretrained/loras/DreamBooth_LoRA/toonyou_beta3.safetensors \
- --dump_path output/lora_convert_check \
+ --checkpoint_path pretrained/loras/DreamBooth_LoRA/toonyou_beta3.safetensors \
+ --dump_path output/lora_convert_check
